5c070cf4572a6f7fbc123095a05eab102358e78f
Bogdan_141 <bogdanmakovey1001@gmail.com>
2025-04-04 13:03:44 +0200

    delete helmet provider

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,13 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <BrowserRouter>
-            <HelmetProvider>
               <App />
-            </HelmetProvider>
           </BrowserRouter>
         </PersistGate>
       </Provider>
