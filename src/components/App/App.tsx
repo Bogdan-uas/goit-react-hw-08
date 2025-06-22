7d641b82c2c5425ca12ee9c8ea95fd093b0ec408
Bogdan_141 <bogdanmakovey1001@gmail.com>
2025-06-22 16:52:22 +0200

    'Fix' a bug with modals and prolly finish big update #2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, lazy, Suspense, ReactElement } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { Toaster } from "react-hot-toast";
 import { useTranslation } from "react-i18next";
@@ -7,102 +7,87 @@ import { useTranslation } from "react-i18next";
 import { selectIsModalOpen, selectDarkMode } from "../../redux/ui/selectors";
 import { apiRefreshUser } from "../../redux/auth/operations";
 
-import LoginPage from "../../pages/LoginPage/LoginPage";
-import RegistrationPage from "../../pages/RegistrationPage/RegistrationPage";
-import ContactsPage from "../../pages/ContactsPage/ContactsPage";
-
 import type { AppDispatch } from "../../redux/store";
 
 import "./App.css";
 
 const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const ContactsPage = lazy(() => import("../../pages/ContactsPage/ContactsPage"));
+const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
+const RegistrationPage = lazy(() => import("../../pages/RegistrationPage/RegistrationPage"));
 const Layout = lazy(() => import("../Layout/Layout"));
 const RestrictedRoute = lazy(() => import("../RestrictedRoute/RestrictedRoute"));
 const PrivateRoute = lazy(() => import("../PrivateRoute/PrivateRoute"));
 
 function App(): ReactElement {
-const dispatch = useDispatch<AppDispatch>();
-const isAnyModalOpen = useSelector(selectIsModalOpen);
-const darkMode = useSelector(selectDarkMode);
-const { t } = useTranslation();
+    const dispatch = useDispatch<AppDispatch>();
+    const isAnyModalOpen = useSelector(selectIsModalOpen);
+    const darkMode = useSelector(selectDarkMode);
+    const { t } = useTranslation();
 
-const handleIsModalOpen = (t: ReactElement, r: ReactElement): ReactElement =>
-    isAnyModalOpen ? r : t;
-    
-useEffect(() => {
-    import('../../pages/RegistrationPage/RegistrationPage');
-    import('../../pages/LoginPage/LoginPage');
-}, []);
+    useEffect(() => {
+        dispatch(apiRefreshUser());
+    }, [dispatch]);
 
-useEffect(() => {
-    dispatch(apiRefreshUser());
-}, [dispatch]);
+    useEffect(() => {
+        document.body.classList.toggle("dark", darkMode);
+    }, [darkMode]);
 
-useEffect(() => {
-    document.body.classList.toggle("dark", darkMode);
-}, [darkMode]);
+    useEffect(() => {
+        import("../../pages/LoginPage/LoginPage");
+        import("../../pages/RegistrationPage/RegistrationPage");
+    }, []);
 
-return (
-    <Suspense fallback={<div>{t("app.loading")}</div>}>
-    <Layout>
-        {handleIsModalOpen(
-        <Toaster
-            position="top-center"
-            reverseOrder={true}
-            toastOptions={{
+    const toasterOptions = {
+        reverseOrder: true,
+        toastOptions: {
             style: {
                 background: "var(--modal_bg)",
                 color: "var(--text)",
                 borderRadius: "10px",
                 boxShadow: "var(--shadow)",
             },
-            }}
-        />,
-        <Toaster
-            position="top-right"
-            reverseOrder={true}
-            toastOptions={{
-            style: {
-                background: "var(--modal_bg)",
-                color: "var(--text)",
-                borderRadius: "10px",
-                boxShadow: "var(--shadow)",
-            },
-            }}
-        />
-        )}
+        },
+    };
 
-        <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route
-            path="/contacts"
-            element={
-            <PrivateRoute>
-                <ContactsPage />
-            </PrivateRoute>
-            }
-        />
-        <Route
-            path="/register"
-            element={
-            <RestrictedRoute>
-                <RegistrationPage />
-            </RestrictedRoute>
-            }
-        />
-        <Route
-            path="/login"
-            element={
-            <RestrictedRoute>
-                <LoginPage />
-            </RestrictedRoute>
-            }
-        />
-        <Route path="*" element={<HomePage />} />
-        </Routes>
-    </Layout>
-    </Suspense>
-);
+    const toasterPosition = isAnyModalOpen ? "top-right" : "top-center";
+
+    return (
+        <Suspense fallback={<div>{t("app.loading")}</div>}>
+            <Layout>
+                <Toaster position={toasterPosition} {...toasterOptions} />
+
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route
+                        path="/contacts"
+                        element={
+                            <PrivateRoute>
+                                <ContactsPage />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/register"
+                        element={
+                            <RestrictedRoute>
+                                <RegistrationPage />
+                            </RestrictedRoute>
+                        }
+                    />
+                    <Route
+                        path="/login"
+                        element={
+                            <RestrictedRoute>
+                                <LoginPage />
+                            </RestrictedRoute>
+                        }
+                    />
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </Layout>
+        </Suspense>
+    );
 }
 
 export default App;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,25 +1,65 @@
+import { ChangeEvent } from "react";
 import { NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
 import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { selectIsModalOpen, selectIsEditingGlobal } from "../../redux/ui/selectors";
 import { useTranslation } from "react-i18next";
 
 export default function Navigation(): React.ReactElement {
+    const isModalOpen = useSelector(selectIsModalOpen);
+    const isEditingGlobal = useSelector(selectIsEditingGlobal);
+    const isLocked = isModalOpen || isEditingGlobal;
     const isLoggedIn = useSelector(selectIsLoggedIn);
     const { t } = useTranslation();
 
     return (
         <div>
             <nav className={css.nav}>
-                <NavLink className={css.link} to="/">
+                <NavLink
+                    className={`${css.link} ${isLocked ? css.disabled : ""}`}
+                    to="/"
+                    onClick={(e) => {
+                        if (isLocked) {
+                            e.preventDefault();
+                            toast.error(
+                                isModalOpen
+                                    ? t("navigation.errors.closeModalFirst")
+                                    : t("navigation.errors.cannotNavigateWhileEditing"),
+                                {
+                                    duration: 4000,
+                                    style: { borderRadius: "10px", textAlign: "center" },
+                                }
+                            );
+                        }
+                    }}
+                >
                     {t("app.navigation.home")}
                 </NavLink>
                 {isLoggedIn && (
-                    <NavLink className={css.link} to="/contacts">
+                    <NavLink
+                        to="/contacts"
+                        className={`${css.link} ${isLocked ? css.disabled : ""}`}
+                        onClick={(e) => {
+                            if (isLocked) {
+                                e.preventDefault();
+                                toast.error(
+                                    isModalOpen
+                                        ? t("navigation.errors.closeModalFirst")
+                                        : t("navigation.errors.cannotNavigateWhileEditing"),
+                                    {
+                                        duration: 4000,
+                                        style: { borderRadius: "10px", textAlign: "center" },
+                                    }
+                                );
+                            }
+                        }}
+                    >
                         {t("app.navigation.contacts")}
                     </NavLink>
                 )}
             </nav>
-        </div>
+        </div >
     );
 }
