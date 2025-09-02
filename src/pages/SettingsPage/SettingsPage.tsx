72f31049f88937fb99f29369d4f16a746426f1d4
Bogdan_141 <bogdanmakovey1001@gmail.com>
2025-09-02 19:12:29 +0200

    add reset button to settings, there are still some bugs, will be fixed later

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,11 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import { setDarkMode } from "../../redux/ui/themeSlice";
 import { useTranslation } from "react-i18next";
+import toast from "react-hot-toast";
 import css from './SettingsPage.module.css';
 import LanguageSelector from "../../components/LanguageSelector/LanguageSelector";
 import ThemeToggle from "../../components/ThemeToggle/ThemeToggle";
 import { Link } from "react-router-dom";
+import { selectDarkMode } from "../../redux/ui/selectors";
 
 const SettingsPage = () => {
-    const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const { i18n, t } = useTranslation();
+    const darkMode = useSelector(selectDarkMode);
+
+    const resetSettings = () => {
+        if (!darkMode && i18n.language === "en") {
+            toast.error(t("settingsPage.toast.alreadyReset"));
+            return;
+        }
+        dispatch(setDarkMode(false));
+        i18n.changeLanguage("en");
+        toast.success(t("settingsPage.toast.resetSuccess"));
+    };
 
     return (
         <div className={css.settingsPage}>
@@ -18,6 +34,10 @@ const SettingsPage = () => {
             </div>
             <LanguageSelector />
             <ThemeToggle />
+            <div className={css.divider} />
+            <button onClick={resetSettings} className={css.reset_button}>
+                {t("settingsPage.resetButton")}
+            </button>
         </div>
     );
 };
